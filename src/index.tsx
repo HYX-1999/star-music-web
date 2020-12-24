5201314691aa1ae2e06e58c6c4a52ad2fbff14c1
HYX-1999 <1502318069@qq.com>
2020-12-24 11:55:26 +0800

    login

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
 import routes from './router'
+import 'antd/dist/antd.css'
 import './styles/app.scss';
 
 ReactDOM.render(
