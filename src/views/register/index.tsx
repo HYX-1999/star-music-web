c0e9e67653a8a2cb3bc045eb7a1059caa3372eec
HYX-1999 <1502318069@qq.com>
2020-12-28 17:49:22 +0800

    注册

--- src/views/register/index.tsx
+++ src/views/register/index.tsx
@@ -1,13 +1,40 @@
-import { Form, Input, Button, InputNumber } from 'antd'
-// import { Link } from 'react-router-dom'
-import { UserOutlined, PhoneOutlined, LockOutlined } from '@ant-design/icons'
+import {
+  Form,
+  Input,
+  Row,
+  Col,
+  Checkbox,
+  Button
+} from 'antd';
 require('./index.scss')
 
-const layout = {
-  labelCol: { offset: 4 },
-  wrapperCol: { span: 16, offset: 4 }
-}
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 20 },
+    sm: { span: 5 },
+  },
+  wrapperCol: {
+    xs: { span: 20 },
+    sm: { span: 15 },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 4,
+    },
+  },
+};
+
 const Register = () => {
+  
+  const [form] = Form.useForm();
+
   const onFinish = values => {
     console.log('Received values of form: ', values);
   };
@@ -18,44 +45,97 @@ const Register = () => {
       <div className="login-content">
         <h2 className="login-title">用户注册</h2>
         <Form
-          { ...layout }
-          name="normal_login"
-          className="login-form"
-          initialValues={{ remember: true }}
+          {...formItemLayout}
+          form={form}
+          name="register"
           onFinish={onFinish}
+          scrollToFirstError
         >
           <Form.Item
             name="nickname"
-            rules={[{ required: true, message: '请输入您的昵称!' }]}
+            label="昵称"
+            rules={[{ required: true, message: '请输入昵称!', whitespace: true }]}
           >
-            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="昵称" />
+            <Input />
           </Form.Item>
           <Form.Item
-            rules={[{ required: true, message: '请输入您的手机号码!' }]}
+            name="phone"
+            label="手机"
+            rules={[{ required: true, message: '请输入手机号码!' }]}
           >
-            <Input prefix={<PhoneOutlined className="site-form-item-icon" />} placeholder="手机号码" />
+            <Input />
+          </Form.Item>
+          <Form.Item label="验证码">
+            <Row gutter={8}>
+              <Col span={8}>
+                <Form.Item
+                  name="captcha"
+                  noStyle
+                  rules={[{ required: true, message: '请输入验证码!' }]}
+                >
+                  <Input />
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Button type="primary">发送验证码</Button>
+              </Col>
+            </Row>
           </Form.Item>
           <Form.Item
             name="password"
-            rules={[{ required: true, message: '请输入密码!' }]}
+            label="密码"
+            rules={[
+              {
+                required: true,
+                message: '请输入密码!',
+              },
+            ]}
+            hasFeedback
           >
-            <Input
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              type="password"
-              placeholder="密码"
-            />
-          </Form.Item>
-          <Form.Item
-            label="验证码"
-            rules={[{ required: true, message: '请输入验证码!' }]}
-          >
-            <InputNumber />
-            <Button>点击发送验证码</Button>
+            <Input.Password />
           </Form.Item>
 
-          <Form.Item className="submitRow">
-            <Button type="primary">
-              注册
+          <Form.Item
+            name="confirm"
+            label="确认密码"
+            dependencies={['password']}
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: '请输入密码!',
+              },
+              ({ getFieldValue }) => ({
+                validator(rule, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject('两次密码不相同!');
+                }
+              }),
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            name="agreement"
+            valuePropName="checked"
+            rules={[
+              {
+                validator: (_, value) =>
+                  value ? Promise.resolve() : Promise.reject('请勾选协议'),
+              },
+            ]}
+            {...tailFormItemLayout}
+          >
+            <Checkbox>
+              I have read the agreement
+            </Checkbox>
+          </Form.Item>
+          <Form.Item {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit">
+              Register
             </Button>
           </Form.Item>
         </Form>
